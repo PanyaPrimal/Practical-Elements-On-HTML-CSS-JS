b12c406b35c2d0e65f564d7b29c6f6c3a1f777c3
Victor <vpanya97@gmail.com>
2021-06-18 10:23:00 +0300

    active toggler for checkboxes

--- Styles-for-form-elements/js/script.js
+++ Styles-for-form-elements/js/script.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
             $(this).addClass('active');
         }
     });
+    $(document).on('click', '.checkbox'. function(e) {
+        if($(this).hasClass('active')){
+            $(this).find('input').prop('checked',false);
+        }else{
+            $(this).find('input').prop('checked',true);
+        }
+        $(this).toggleClass('active');
+        
+        return false;
+    });
 });
