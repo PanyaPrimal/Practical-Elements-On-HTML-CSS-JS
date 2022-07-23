8bd8f96ba30002d6019c7118412cba47c1f0ef26
Victor <vpanya97@gmail.com>
2022-07-23 08:54:58 +0300

    add class ColorPalette

--- paintjs-master/js/color-palette.js
+++ paintjs-master/js/color-palette.js
@@ -1,3 +1,34 @@
 class ColorPalette {
-    
+    constructor({ element, colors = [] }) {
+        this.element = element;
+        this.colorElements = [];
+
+        this.colors = colors;
+        this.currentColor = null;
+
+        this.init();
+    }
+
+    init() {
+        this.element.addEventListener('click', this.handColorSelected.bind(this));
+
+        for (let color of this.colors) {
+            let li = document.createElement('li');
+            li.className = 'color-palette__color';
+            li.style.backgroundColor = `rgb(${color.red},${color.green},${color.blue})`;
+            this.element.appendChild(li);
+            this.colorElements.push(li);
+        }
+    }
+    handColorSelected({target}) {
+        if (target.tagName === 'LI') {
+            this.currentColor = target.style.backgroundColor;
+
+            for (let colorElement of this.colorElements) {
+                colorElement.classList.remove('selected')
+            }
+
+            target.classList.add('selected')
+        }
+    }
 }
